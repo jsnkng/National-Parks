73361158021ec219c5367c4b7f20c3c5cfb7a481
Jason King <jking@cab408.com>
2019-11-26 11:05:45 -0500

    remove dotenv

--- next.config.js
+++ next.config.js
@@ -1,13 +1,1 @@
 // next.config.js
-
-const { parsed: localEnv } = require('dotenv').config()
-const webpack = require('webpack')
-
-module.exports = {
-  exportTrailingSlash: true,
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
-
-    return config
-  }
-}
