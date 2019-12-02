cc60609ff4053a1d6bbac916990d18af217cc3e7
Jason King <jking@cab408.com>
2019-12-02 17:44:46 -0500

    Update configuration

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 // next.config.js
+require('dotenv').config()
 
-// const { parsed: localEnv } = require('dotenv').config()
-// const webpack = require('webpack')
-
-// module.exports = {
-//   exportTrailingSlash: true,
-//   webpack(config) {
-//     config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
-
-//     return config
-//   }
-// }
+module.exports = {
+  env: {
+    NPS_URI: process.env.NPS_URI,
+    NPS_KEY: process.env.NPS_KEY,
+    MONGODB_URI: process.env.MONGODB_URI,
+    DB_NAME: process.env.DB_NAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    WEB_URI: process.env.WEB_URI
+  }
+}
