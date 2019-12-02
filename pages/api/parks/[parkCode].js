cf415373953d8d9bc322b8ac823f242745054877
Jason King <jking@cab408.com>
2019-12-02 17:44:11 -0500

    Add database middleware

--- pages/api/parks/[parkCode].js
+++ pages/api/parks/[parkCode].js
@@ -1,156 +1,73 @@
-import fetch from 'isomorphic-unfetch';
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+import fetch from 'isomorphic-unfetch'
+import nextConnect from 'next-connect'
+import database from '../../../middlewares/database'
 
-const NPS_KEY="O5YBusXqpWGTqfOUMaeMNBg6oGfUdeh4vYzjBRvj"
-const NPS_API="https://developer.nps.gov/api/v1"
-const DB_URL="mongodb+srv://national_parks:1Y5QMu6632vZ3QtF@cluster0-pexqw.mongodb.net/NationalParkService_Cache?retryWrites=true&w=majority"
+const handler = nextConnect()
 
+handler.use(database)
 
-// NPS API
-// const NPS_API = process.env.NPS_API
-// const NPS_KEY = process.env.NPS_KEY
-
-// Connection URL
-// const DB_URL = process.env.DB_URL
-
-
-// Database Name
-const dbName = 'NationalParkService_Cache';
-const client = new MongoClient(DB_URL, { useNewUrlParser: true , useUnifiedTopology: true });
-
-
-
-
-export default (req, res) => {
+handler.get(async (req, res) => {
   const {
-    query: { parkCode },
-    method
+    query: { parkCode }
   } = req
+  let [parks] = await req.db.collection('parks').find({ parks_id: parkCode }).toArray()
 
-
-  // Use connect method to connect to the server
-  client.connect(function(err) {
-    assert.equal(null, err)
-    console.log('Connected successfully to server')
-
-    const db = client.db(dbName)
-
-    getParksFromCache(db, parkCode, function(cachedParks) {
-        if(cachedParks === undefined || cachedParks.length == 0)  {
-            console.log('ERROR: Could not get parks from MongoDB cache')
-            
-            
-            fetchParksFromAPI(parkCode, function(fetchedParks) {
-                if(fetchedParks === undefined || fetchedParks.length == 0)  {
-                    console.log('ERROR: Could not fetch parks from NPS API')
-                } else {
-                    console.log('SUCCESS: Fetched parks from NPS API')
-                    
-                    writeParksToCache(db, parkCode, fetchedParks, function(newCachedParks) {
-                      if(newCachedParks === undefined || newCachedParks.length == 0)  {
-                        console.log('ERROR: Could not write parks to MongoDB cache')
-                      } else {
-                        console.log('SUCCESS: Wrote parks to MongoDB cache')
-                        res.status(200).json(newCachedParks[0])
-                      }
-                    })
-
-                }
-            })
-
-
-        } else {
-          console.log('SUCCESS: Got parks from MongoDB cache')
-          res.status(200).json(cachedParks[0])
-        }
-
-
-    })
-
-  })
-}
-
-
-const getParksFromCache = function(db, parkCode, callback) {
-  // Get the parks from MongoDB cache
-  const collection = db.collection('parks')
-  // Find the parks matching parks_id
-  collection.find({'parks_id': parkCode}).toArray(function(err, parks) {
-    assert.equal(err, null)
-    // console.log(parks)
-    callback(parks)
-  });
-}
-
-const fetchParksFromAPI = function(parkCode, callback) {
-
-  const data = {}
-  // Fetch the parks from the NPS API
-  fetch(`${NPS_API}/parks?parkCode=${parkCode}&fields=images&api_key=${NPS_KEY}`)
-  .then( r => r.json() )
-  .then( parks => {
-    data.parks = parks
-    fetch(`${NPS_API}/people?parkCode=${parkCode}&fields=images&api_key=${NPS_KEY}`)
-    .then( r => r.json() )
-    .then( people => {
-      data.people = people
-      fetch(`${NPS_API}/places?parkCode=${parkCode}&fields=images&api_key=${NPS_KEY}`)
-      .then( r => r.json() )
-      .then( places => {
-        data.places = places
-        fetch(`${NPS_API}/visitorcenters?parkCode=${parkCode}&fields=images&api_key=${NPS_KEY}`)
-        .then( r => r.json() )
-        .then( visitorcenters => {
-          data.visitorcenters = visitorcenters
-          fetch(`${NPS_API}/events?parkCode=${parkCode}&fields=images&api_key=${NPS_KEY}`)
-          .then( r => r.json() )
-          .then( events => {
-            data.events = events
-            fetch(`${NPS_API}/articles?parkCode=${parkCode}&fields=images&api_key=${NPS_KEY}`)
-            .then( r => r.json() )
-            .then( articles => {
-              data.articles = articles
-              fetch(`${NPS_API}/alerts?parkCode=${parkCode}&fields=images&api_key=${NPS_KEY}`)
-              .then( r => r.json() )
-              .then( alerts => {
-                data.alerts = alerts
-                fetch(`${NPS_API}/campgrounds?parkCode=${parkCode}&fields=images&api_key=${NPS_KEY}`)
-                .then( r => r.json() )
-                .then( campgrounds => {
-                  data.campgrounds = campgrounds
-                  fetch(`${NPS_API}/newsreleases?parkCode=${parkCode}&fields=images&api_key=${NPS_KEY}`)
-                  .then( r => r.json() )
-                  .then( newsreleases => {
-                    data.newsreleases = newsreleases
-  
-  
-                    
-                    console.log(data)
-                    callback(data)
-                  })
-                })
-              })
-            })
-          })
-        })
+  if (parks === undefined || parks.length === 0) {
+    console.log('[1]', parks)
+    parks = await fetch(`${process.env.NPS_URI}/parks?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
+      .then(fetchResponse => fetchResponse.json())
+      .catch(err => {
+        console.log(err.stack)
       })
-    })
-  })
-}
+    parks.people = await fetch(`${process.env.NPS_URI}/people?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
+      .then(fetchResponse => fetchResponse.json())
+      .catch(err => {
+        console.log(err.stack)
+      })
+    parks.places = await fetch(`${process.env.NPS_URI}/places?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
+      .then(fetchResponse => fetchResponse.json())
+      .catch(err => {
+        console.log(err.stack)
+      })
+    parks.visitorcenters = await fetch(`${process.env.NPS_URI}/visitorcenters?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
+      .then(fetchResponse => fetchResponse.json())
+      .catch(err => {
+        console.log(err.stack)
+      })
+    parks.events = await fetch(`${process.env.NPS_URI}/events?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
+      .then(fetchResponse => fetchResponse.json())
+      .catch(err => {
+        console.log(err.stack)
+      })
+    parks.articles = await fetch(`${process.env.NPS_URI}/articles?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
+      .then(fetchResponse => fetchResponse.json())
+      .catch(err => {
+        console.log(err.stack)
+      })
+    parks.alerts = await fetch(`${process.env.NPS_URI}/alerts?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
+      .then(fetchResponse => fetchResponse.json())
+      .catch(err => {
+        console.log(err.stack)
+      })
+    parks.campgrounds = await fetch(`${process.env.NPS_URI}/campgrounds?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
+      .then(fetchResponse => fetchResponse.json())
+      .catch(err => {
+        console.log(err.stack)
+      })
+    parks.newsreleases = await fetch(`${process.env.NPS_URI}/newsreleases?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
+      .then(fetchResponse => fetchResponse.json())
+      .catch(err => {
+        console.log(err.stack)
+      })
 
-const writeParksToCache = function(db, parkCode, parks, callback) {
-  // Write the parks to the MongoDB cache
+
     parks.parks_id = parkCode
 
-    // Get the parkss collection
-    const collection = db.collection('parks')
-    // Insert parks
-    collection.insertOne(parks, function(err, result) {
-      assert.equal(err, null)
-      assert.equal(1, result.result.n)
-      assert.equal(1, result.ops.length)
-      console.log('Inserted 1 parks into the collection')
-      callback([parks])
-    })
-}
+    console.log('[2]', parks)
+    await req.db.collection('parks').insertOne(parks)
+  }
+
+  res.json(parks)
+})
+
+export default handler
--- pages/api/state/[stateCode].js
+++ pages/api/state/[stateCode].js
@@ -1,113 +1,28 @@
 import fetch from 'isomorphic-unfetch'
-const MongoClient = require('mongodb').MongoClient
-const assert = require('assert')
+import nextConnect from 'next-connect'
+import database from '../../../middlewares/database'
 
-const NPS_KEY="O5YBusXqpWGTqfOUMaeMNBg6oGfUdeh4vYzjBRvj"
-const NPS_API="https://developer.nps.gov/api/v1"
-const DB_URL="mongodb+srv://national_parks:1Y5QMu6632vZ3QtF@cluster0-pexqw.mongodb.net/NationalParkService_Cache?retryWrites=true&w=majority"
+const handler = nextConnect()
 
-// NPS API
-// const NPS_API = process.env.NPS_API
-// const NPS_KEY = process.env.NPS_KEY
+handler.use(database)
 
-
-// // Connection URL
-// const DB_URL = process.env.DB_URL
-
-// Database Name
-const dbName = 'NationalParkService_Cache'
-const client = new MongoClient(DB_URL, { useNewUrlParser: true , useUnifiedTopology: true })
-
-
-
-
-export default (req, res) => {
+handler.get(async (req, res) => {
   const {
-    query: { stateCode },
-    method
+    query: { stateCode }
   } = req
+  let [state] = await req.db.collection('states').find({ state_id: stateCode }).toArray()
 
-
-  // Use connect method to connect to the server
-  client.connect(function(err) {
-    assert.equal(null, err)
-    console.log('Connected successfully to server')
-
-    const db = client.db(dbName)
-
-    getStateFromCache(db, stateCode, function(cachedState) {
-        if(cachedState === undefined || cachedState.length == 0)  {
-            console.log('ERROR: Could not get state from MongoDB cache')
-            
-            
-            fetchStateFromAPI(stateCode, function(fetchedState) {
-                if(fetchedState === undefined || fetchedState.length == 0)  {
-                    console.log('ERROR: Could not fetch state from NPS API')
-                } else {
-                    console.log('SUCCESS: Fetched state from NPS API')
-                    
-                    writeStateToCache(db, stateCode, fetchedState, function(newCachedState) {
-                      if(newCachedState === undefined || newCachedState.length == 0)  {
-                        console.log('ERROR: Could not write state to MongoDB cache')
-                      } else {
-                        console.log('SUCCESS: Wrote state to MongoDB cache')
-                        res.status(200).json(newCachedState[0])
-                      }
-                    })
-
-                }
-            })
-
-
-        } else {
-          console.log('SUCCESS: Got state from MongoDB cache')
-          res.status(200).json(cachedState[0])
-        }
-
-
-    })
-
-  })
-}
-
-const getStateFromCache = function(db, stateCode, callback) {
-  // Get the state from MongoDB cache
-  const collection = db.collection('states')
-  // Find the state matching state_id
-  collection.find({'state_id': stateCode}).toArray(function(err, state) {
-    assert.equal(err, null)
-    console.log(state)
-    callback(state)
-  });
-}
-
-const fetchStateFromAPI = function(stateCode, callback) {
-  // Fetch the state from the NPS API
-  fetch(`${NPS_API}/parks?stateCode=${stateCode}&fields=images&api_key=${NPS_KEY}`)
-    .then( r => r.json() )
-    .then( state => {
-      // console.log(state)
-      callback(state)
-    })
-    .catch(err => {
-      console.log(err.stack)
-    })
-}
-
-const writeStateToCache = function(db, stateCode, state, callback) {
-  // Write the state to the MongoDB cache
+  if (state === undefined || state.length === 0) {
+    state = await fetch(`${process.env.NPS_URI}/parks?stateCode=${stateCode}&fields=images&api_key=${process.env.NPS_KEY}`)
+      .then(fetchResponse => fetchResponse.json())
+      .catch(err => {
+        console.log(err.stack)
+      })
     state.state_id = stateCode
+    await req.db.collection('states').insertOne(state)
+  }
 
-    // Get the states collection
-    const collection = db.collection('states')
-    // Insert state
-    collection.insertOne(state, function(err, result) {
-      assert.equal(err, null)
-      assert.equal(1, result.result.n)
-      assert.equal(1, result.ops.length)
-      console.log('Inserted 1 state into the collection')
-      callback([state])
-    })
-}
+  res.json(state)
+})
 
-
+export default handler
