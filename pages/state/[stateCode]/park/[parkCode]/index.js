a5837bf057e92f3101cc297714ad4db10c9a3dd1
Jason King <jking@cab408.com>
2019-12-23 20:22:07 -0500

    test

--- pages/state/[stateCode]/park/[parkCode]/index.js
+++ pages/state/[stateCode]/park/[parkCode]/index.js
@@ -54,17 +54,17 @@ const Park = props => {
        <script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=Intl.~locale.en"></script>
     </Head>
     <Container>
-      {/* <Masthead__Component 
+      <Masthead__Component 
         pageTitle={park.name}
         pageStateCode={stateCode}
         pageSubTitle={park.designation}
         pageSubSubTitle={states[stateCode][0]}
         pageSubSubSubTitle="" 
-      /> */}
+      />
 
-      {/* { park.images !== undefined && park.images.length !== 0 &&
+      { park.images !== undefined && park.images.length !== 0 &&
         <SlideShow__Component park={park} />
-      } */}
+      }
 
 
       <Description__Wrapper>
@@ -74,10 +74,10 @@ const Park = props => {
           }
           {/* <a href="{park.url}">National Park Service’s {park.name} Website.</a> */}
       </Description__Wrapper>
-      
-      {/* <VisitorInfo__Wrapper>
+{/*       
+      <VisitorInfo__Wrapper>
         <VisitorInfo__Component park={park} markers={markers} />
-      </VisitorInfo__Wrapper> */}
+      </VisitorInfo__Wrapper>
       
       { events !== undefined && events.length != 0 &&
         <Events__Wrapper>
@@ -129,14 +129,14 @@ const Park = props => {
         <People__Wrapper>
           <People__Component park={park} people={people} />
         </People__Wrapper>
-      }
-      {/* <Footer__Component
+      } */}
+      <Footer__Component
         pageTitle={park.name}
         pageStateCode={stateCode}
         pageSubTitle={park.designation}
         pageSubSubTitle={states[stateCode][0]}
         pageSubSubSubTitle=""
-       /> */}
+       />
     </Container>
     </>
   )
