cea26a070c8e8d245f33c97c2d2866cf731dffad
Jason King <jking@cab408.com>
2019-12-25 10:01:21 -0500

    park api speed

--- pages/api/parks/[parkCode].js
+++ pages/api/parks/[parkCode].js
@@ -5,6 +5,14 @@ import s3Images from '../_utils/_s3Images'
 
 const handler = nextConnect()
 
+const fetchWithErrorHandling = async url => {
+  try {
+    return await (await fetch(url)).json()
+  } catch (err) {
+    return { error: true }
+  }
+}
+
 handler.use(database)
 
 handler.get(async (req, res) => {
@@ -13,43 +21,19 @@ handler.get(async (req, res) => {
   } = req
   let [parks] = await req.db.collection('parks').find({ parks_id: parkCode }).toArray()
 
-  const [alerts] = await req.db.collection('alerts').find({ parks_id: parkCode }).toArray()
-  const [events] = await req.db.collection('events').find({ parks_id: parkCode }).toArray()
-  const [newsreleases] = await req.db.collection('newsreleases').find({ parks_id: parkCode }).toArray()
-  const [articles] = await req.db.collection('articles').find({ parks_id: parkCode }).toArray()
-
   if (parks === undefined || parks.length === 0) {
     console.log('parks was undefined or length 0')
-    parks = await fetch(`${process.env.NPS_URI}/parks?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
-      .then(fetchResponse => fetchResponse.json())
-      .catch(err => {
-        console.log(err.stack)
-      })
-    parks.people = await fetch(`${process.env.NPS_URI}/people?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
-      .then(fetchResponse => fetchResponse.json())
-      .catch(err => {
-        console.log(err.stack)
-      })
-    parks.places = await fetch(`${process.env.NPS_URI}/places?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
-      .then(fetchResponse => fetchResponse.json())
-      .catch(err => {
-        console.log(err.stack)
-      })
-    parks.visitorcenters = await fetch(`${process.env.NPS_URI}/visitorcenters?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
-      .then(fetchResponse => fetchResponse.json())
-      .catch(err => {
-        console.log(err.stack)
-      })
-    parks.articles = await fetch(`${process.env.NPS_URI}/articles?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
-      .then(fetchResponse => fetchResponse.json())
-      .catch(err => {
-        console.log(err.stack)
-      })
-    parks.campgrounds = await fetch(`${process.env.NPS_URI}/campgrounds?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
-      .then(fetchResponse => fetchResponse.json())
-      .catch(err => {
-        console.log(err.stack)
-      })
+    parks = await fetchWithErrorHandling(`${process.env.NPS_URI}/parks?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
+
+    parks.people = await fetchWithErrorHandling(`${process.env.NPS_URI}/people?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
+
+    parks.places = await fetchWithErrorHandling(`${process.env.NPS_URI}/places?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
+
+    parks.visitorcenters = await fetchWithErrorHandling(`${process.env.NPS_URI}/visitorcenters?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
+
+    parks.articles = await fetchWithErrorHandling(`${process.env.NPS_URI}/articles?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
+
+    parks.campgrounds = await fetchWithErrorHandling(`${process.env.NPS_URI}/campgrounds?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
 
     parks.parks_id = parkCode
 
@@ -60,12 +44,11 @@ handler.get(async (req, res) => {
     await s3Images(parks.data[0].images)
   }
 
+
+  const [alerts] = await req.db.collection('alerts').find({ parks_id: parkCode }).toArray()
   if (alerts === undefined || alerts.length === 0) {
-    parks.alerts = await fetch(`${process.env.NPS_URI}/alerts?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
-      .then(fetchResponse => fetchResponse.json())
-      .catch(err => {
-        console.log(err.stack)
-      })
+    parks.alerts = await fetchWithErrorHandling(`${process.env.NPS_URI}/alerts?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
+
     console.log('Geet Alerts')
     parks.alerts.parks_id = parkCode
     await req.db.collection('alerts').insertOne(parks.alerts)
@@ -73,12 +56,10 @@ handler.get(async (req, res) => {
     parks.alerts = alerts
   }
 
+  const [events] = await req.db.collection('events').find({ parks_id: parkCode }).toArray()
   if (events === undefined || events.length === 0) {
-    parks.events = await fetch(`${process.env.NPS_URI}/events?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
-      .then(fetchResponse => fetchResponse.json())
-      .catch(err => {
-        console.log(err.stack)
-      })
+    parks.events = await fetchWithErrorHandling(`${process.env.NPS_URI}/events?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
+
     console.log('Geet Events')
     parks.events.parks_id = parkCode
     await req.db.collection('events').insertOne(parks.events)
@@ -86,12 +67,11 @@ handler.get(async (req, res) => {
     parks.events = events
   }
 
+  const [newsreleases] = await req.db.collection('newsreleases').find({ parks_id: parkCode }).toArray()
+
   if (newsreleases === undefined || newsreleases.length === 0) {
-    parks.newsreleases = await fetch(`${process.env.NPS_URI}/newsreleases?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
-      .then(fetchResponse => fetchResponse.json())
-      .catch(err => {
-        console.log(err.stack)
-      })
+    parks.newsreleases = await fetchWithErrorHandling(`${process.env.NPS_URI}/newsreleases?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
+
     console.log('Geet newsreleases')
     parks.newsreleases.parks_id = parkCode
     await req.db.collection('newsreleases').insertOne(parks.newsreleases)
@@ -99,12 +79,10 @@ handler.get(async (req, res) => {
     parks.newsreleases = newsreleases
   }
 
+  const [articles] = await req.db.collection('articles').find({ parks_id: parkCode }).toArray()
   if (articles === undefined || articles.length === 0) {
-    parks.articles = await fetch(`${process.env.NPS_URI}/articles?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
-      .then(fetchResponse => fetchResponse.json())
-      .catch(err => {
-        console.log(err.stack)
-      })
+    parks.articles = await fetchWithErrorHandling(`${process.env.NPS_URI}/articles?parkCode=${parkCode}&fields=images&api_key=${process.env.NPS_KEY}`)
+
     console.log('Geet articles')
     parks.articles.parks_id = parkCode
     await req.db.collection('articles').insertOne(parks.articles)
