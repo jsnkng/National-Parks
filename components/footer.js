a6bcadbbb428aa748f0e0b204dc7a8951274c8eb
Jason King <jking@cab408.com>
2020-01-07 15:18:41 -0500

    test icons

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,10 @@ import styled from 'styled-components'
 import Copyright from '../components/copyright'
 import ThemeSwitcher from '../components/themeswitcher'
 import SuperQuery from '@themgoncalves/super-query'
-
+import {Arch} from 'styled-icons/boxicons-regular/Arch'
+import {Compass} from 'styled-icons/fa-regular/Compass'
+import {Home} from 'styled-icons/boxicons-regular/Home'
+import {Lab} from 'styled-icons/icomoon/Lab'
 const Component = ({ themeName, setThemeName }) => {
   return (
     <Footer>
@@ -13,18 +16,22 @@ const Component = ({ themeName, setThemeName }) => {
       </Copyright__Container> */}
       <Buttons>
       <Button__Container>
+        <MyHome /><br />
         <Link href='/'>Home</Link>
       </Button__Container>
 
       <Button__Container>
+        <MyCompass /><br />
         <Link href='/states'>States</Link>
       </Button__Container>
 
       <Button__Container>
+        <MyArch /><br />
         <Link href='/designations'>Designations</Link>
       </Button__Container>
 
       <Button__Container>
+        <MyLab /><br />
         <Link href='/about'>About</Link>
       </Button__Container>
 </Buttons>
@@ -36,22 +43,41 @@ const Component = ({ themeName, setThemeName }) => {
 }
 
 export default Component
-
+const MyHome = styled(Home)`
+  color: black;
+  width: 25px;
+  text-align:center;
+`
+const MyArch = styled(Arch)`
+  color: black;
+  width: 25px;
+  text-align:center;
+`
+const MyCompass = styled(Compass)`
+  color: black;
+  width: 20px;
+  text-align:center;
+`
+const MyLab = styled(Lab)`
+  color: black;
+  width: 23px;
+  text-align:center;
+`
 const Footer = styled.footer`
   position: fixed;
   bottom: 0;
   left: 0;
   right: 0;
   margin: 0;
-  height: 60px;
+  height: 70px;
   background-color: ${({ theme }) => theme.colors.trans_back};
   color: ${({ theme }) => theme.colors.text};
   z-index: 1000;
   ${SuperQuery().minWidth.sm.css`
-  height: 70px;
+  height: 80px;
   `}
   ${SuperQuery().minWidth.lg.css`
-  height: 80px;
+  height: 90px;
   `}
 `
 
@@ -67,6 +93,7 @@ const Buttons = styled.div`
 `
 const Button__Container = styled.div`
   display:block;
+  text-align:center;
   float: left;
   height:2em;
   font-size: .875em;
