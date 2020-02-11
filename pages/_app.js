825322df9964214b4bf71a4938003c9cce40db8e
Jason King <jking@cab408.com>
2020-02-10 19:48:47 -0500

    add head

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Head from 'next/head'
 import Router from 'next/router'
 import App from 'next/app'
 import { PageTransition } from 'next-page-transitions'
