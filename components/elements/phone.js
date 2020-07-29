13c07190307bb3413bdb78336da660c80686fde6
Jason King <jking@cab408.com>
2020-07-29 11:06:50 -0400

    test for phone number

--- components/elements/phone.js
+++ components/elements/phone.js
@@ -1,11 +1,13 @@
 function formatPhoneNumber(phoneNumberString) {
-  const nospace = phoneNumberString.replace(/\s/g, '')
-  const split1 = nospace.split('/')
-  const split2 = split1[0].split('x')
-  const cleaned = split2[0].toString().replace(/\D/g, '')
-  const match = cleaned.match(/^1?(\d{3})(\d{3})(\d{4})$/)
-  if (match) {
-    return `(${match[1]}) ${match[2]}-${match[3]}`
+  if(typeof phoneNumberString !== undefined && phoneNumberString !== null) {
+    const nospace = phoneNumberString.replace(/\s/g, '')
+    const split1 = nospace.split('/')
+    const split2 = split1[0].split('x')
+    const cleaned = split2[0].toString().replace(/\D/g, '')
+    const match = cleaned.match(/^1?(\d{3})(\d{3})(\d{4})$/)
+    if (match) {
+      return `(${match[1]}) ${match[2]}-${match[3]}`
+    }
   }
   return null
 }
